4b3ad49ca1ca0115ea1e7b62edad95282a3514c7
unknown <binormus006@gmail.com>
2022-07-14 18:43:53 +0545

    third

--- sbgs/urls.py
+++ sbgs/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import url
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.hello,name = 'SBGS'),
+    url(r'^media/(?P<path>.*)$',{'document_root':settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', {'document_root': settings.STATIC_ROOT}), 
    
 ]
 urlpatterns += staticfiles_urlpatterns()

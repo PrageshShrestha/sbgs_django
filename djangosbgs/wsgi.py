f86408c8c46d3944338a6f24d6d3e401c097ecb7
unknown <binormus006@gmail.com>
2022-07-14 20:38:46 +0545

    first

--- djangosbgs/wsgi.py
+++ djangosbgs/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_sbgs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangosbgs.settings')
 
 application = get_wsgi_application()

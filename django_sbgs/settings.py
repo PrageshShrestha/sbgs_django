01c0042dbe55db94b77cfa85269f7c5f2e55d050
unknown <binormus006@gmail.com>
2022-07-14 08:59:25 +0545

    hello

--- django_sbgs/settings.py
+++ django_sbgs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-002)#$+hy1!r6d)-iwe024g(anj5x*z&o^0qz5uj9+bwz06_zn
 DEBUG =False
 
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1' , 'prageshshrestha.pythonanywhere.com']
 
 
 
@@ -126,11 +126,9 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR , 'static')
+STATIC_URL = '/static/'
+# STATICFILES_DIR= [os.path.join(BASE_DIR , 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
